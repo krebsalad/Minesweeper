54e1f8487ca0ed05b1e3e9d64b7b6055d5f31a94
krebsalad <johanmgina7@gmail.com>
2019-11-02 23:45:44 +0100

    Update main.cpp

--- MineSweeper1.0/main.cpp
+++ MineSweeper1.0/main.cpp
@@ -3,12 +3,6 @@
 #include <iostream>
 
 int main(int argc, const char** argv) {
-
-    //uNode::createNodes2d(10, 10);
-    //uNode node = uNode::getNode2d(5,5);
-    //std::cout << "adjacency list for node " << node.nodeId << std::endl;
-    
-
     Minesweeper::Game g(20,20);
     g.init();
     g.run();
